7f34f13dc29f877d4c93e25f31a775ef494d8ebe
Bill Morgan <arthurwilliammorgan@gmail.com>
2026-01-25 10:24:38 -0600

    wip

--- build-ucrt-x86.sh
+++ build-ucrt-x86.sh
@@ -37,4 +37,5 @@ export LLVM_REPOSITORY LLVM_VERSION CORES TOOLCHAIN_ARCHS
 # Stage 2: Windows-hosted toolchain (.exe, unprefixed tools) bootstrapped from stage 1
 "./build-cross-tools.sh" "$NATIVE_PREFIX" "$CROSS_PREFIX" "$CROSS_ARCH" \
   --disable-lldb \
+  --with-clang \
   --thinlto 
