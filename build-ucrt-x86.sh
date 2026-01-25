1aca1c636ee7c9d08323cffd4fb884039082b8db
Bill Morgan <arthurwilliammorgan@gmail.com>
2026-01-25 12:36:03 -0600

    wip

--- build-ucrt-x86.sh
+++ build-ucrt-x86.sh
@@ -20,10 +20,6 @@ export TOOLCHAIN_ARCHS="i686 x86_64"
 export LLVM_REPOSITORY=https://github.com/swiftlang/llvm-project.git
 export LLVM_VERSION=stable/21.x
 export CORES=16
-# We set _WIN32_WINNT to 0x0A00 (Win10)
-# We set NTDDI_VERSION to 0x0A000006 (Win10 1809) to satisfy the >= 0x0A000002 check in minwinbase.h
-export CFLAGS="$CFLAGS -D_WIN32_WINNT=0x0A00 -DNTDDI_VERSION=0x0A000006"
-export CXXFLAGS="$CXXFLAGS -D_WIN32_WINNT=0x0A00 -DNTDDI_VERSION=0x0A000006"
 
 # Stage 1: Linux-hosted toolchain that targets Windows
 NATIVE_PREFIX="$(pwd)/install/llvm-mingw-native"
@@ -35,6 +31,10 @@ DEFAULT_MSVCRT=ucrt
   --thinlto
 
 # Stage 2: Windows-hosted toolchain (.exe) bootstrapped from stage 1
+# We set _WIN32_WINNT to 0x0A00 (Win10)
+# We set NTDDI_VERSION to 0x0A000006 (Win10 1809) to satisfy the >= 0x0A000002 check in minwinbase.h
+export CFLAGS="$CFLAGS -D_WIN32_WINNT=0x0A00 -DNTDDI_VERSION=0x0A000006"
+export CXXFLAGS="$CXXFLAGS -D_WIN32_WINNT=0x0A00 -DNTDDI_VERSION=0x0A000006"
 CROSS_ARCH="x86_64"
 CROSS_PREFIX="$(pwd)/install/llvm-mingw-windows"
 "./build-cross-tools.sh" "$NATIVE_PREFIX" "$CROSS_PREFIX" "$CROSS_ARCH" \
