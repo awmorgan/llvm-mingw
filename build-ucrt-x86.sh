7c914015f13aaf8d648ce7d6e3472b0d78ea03dc
Bill Morgan <arthurwilliammorgan@gmail.com>
2026-01-25 07:11:55 -0600

    wip

--- build-ucrt-x86.sh
+++ build-ucrt-x86.sh
@@ -18,10 +18,20 @@ set -e
 
 # DEST="./install/llvm-mingw"
 
+# CORES=16 \
+# TOOLCHAIN_ARCHS="i686 x86_64" \
+# "./build-all.sh" "./install/llvm-mingw" \
+#   --with-default-msvcrt=ucrt \
+#   --disable-lldb \
+#   --disable-lldb-mi \
+#   --disable-clang-tools-extra
+
+LLVM_REPOSITORY=https://github.com/swiftlang/llvm-project.git \
+LLVM_VERSION=stable/21.x \
 CORES=16 \
 TOOLCHAIN_ARCHS="i686 x86_64" \
 "./build-all.sh" "./install/llvm-mingw" \
   --with-default-msvcrt=ucrt \
-  --disable-lldb \
-  --disable-lldb-mi \
-  --disable-clang-tools-extra
+  --host-clang=clang \
+  --with-clang \
+  --thinlto 
