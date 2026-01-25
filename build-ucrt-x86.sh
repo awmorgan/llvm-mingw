992c62c598d1a96144484ffbaf00e7fe857479cb
Bill Morgan <arthurwilliammorgan@gmail.com>
2026-01-25 09:01:32 -0600

    wip

--- build-ucrt-x86.sh
+++ build-ucrt-x86.sh
@@ -15,13 +15,22 @@ done
 # Redirect all output to both console and log file
 exec > >(tee "$LOG_DIR/build-ucrt-x86.log") 2>&1
 
+NATIVE_PREFIX="./install/llvm-mingw-native"
+CROSS_PREFIX="./install/llvm-mingw-windows"
+NATIVE_ARCHS="i686 x86_64"
+
 LLVM_REPOSITORY=https://github.com/swiftlang/llvm-project.git \
 LLVM_VERSION=stable/21.x \
 CORES=16 \
-TOOLCHAIN_ARCHS="i686 x86_64" \
-"./build-all.sh" "./install/llvm-mingw" \
+TOOLCHAIN_ARCHS="$NATIVE_ARCHS" \
+"./build-all.sh" "$NATIVE_PREFIX" \
   --with-default-msvcrt=ucrt \
-  --host=x86_64-w64-mingw32 \
   --host-clang=clang \
+  --with-clang \
+  --thinlto \
+
+# Build a Windows-hosted toolchain (binaries with .exe and unprefixed tools)
+TOOLCHAIN_ARCHS="$NATIVE_ARCHS" \
+"./build-cross-tools.sh" "$NATIVE_PREFIX" "$CROSS_PREFIX" x86_64 \
   --with-clang \
   --thinlto 
