2a4ff735150535aee0d4d443edd8b88475bc2edd
Bill Morgan <arthurwilliammorgan@gmail.com>
2026-01-25 07:18:12 -0600

    wip

--- build-ucrt-x86.sh
+++ build-ucrt-x86.sh
@@ -1,30 +1,19 @@
 #!/bin/sh
 set -e
 
-# Build llvm-mingw for UCRT i686/x86_64 only, without lldb/lldb-mi/clang-tools-extra.
-# Installs into ./install/llvm-mingw relative to the repo root.
+# Setup rolling log files
+LOG_DIR="logs"
+mkdir -p "$LOG_DIR"
+MAX_LOGS=10
 
-# LLVM_REPOSITORY=https://github.com/swiftlang/llvm-project.git
-# LLVM_VERSION=stable/21.x
-#REPO_ROOT=$(cd "$(dirname "$0")" && pwd)
-# DEST="$REPO_ROOT/install/llvm-mingw"
+# Rotate existing logs
+for i in $(seq $((MAX_LOGS-1)) -1 1); do
+    [ -f "$LOG_DIR/build-ucrt-x86.$i.log" ] && mv "$LOG_DIR/build-ucrt-x86.$i.log" "$LOG_DIR/build-ucrt-x86.$((i+1)).log"
+done
+[ -f "$LOG_DIR/build-ucrt-x86.log" ] && mv "$LOG_DIR/build-ucrt-x86.log" "$LOG_DIR/build-ucrt-x86.1.log"
 
-# TOOLCHAIN_ARCHS="i686 x86_64" \
-# "$REPO_ROOT/build-all.sh" "$DEST" \
-#   --with-default-msvcrt=ucrt \
-#   --disable-lldb \
-#   --disable-lldb-mi \
-#   --disable-clang-tools-extra
-
-# DEST="./install/llvm-mingw"
-
-# CORES=16 \
-# TOOLCHAIN_ARCHS="i686 x86_64" \
-# "./build-all.sh" "./install/llvm-mingw" \
-#   --with-default-msvcrt=ucrt \
-#   --disable-lldb \
-#   --disable-lldb-mi \
-#   --disable-clang-tools-extra
+# Redirect all output to both console and log file
+exec > >(tee "$LOG_DIR/build-ucrt-x86.log") 2>&1
 
 LLVM_REPOSITORY=https://github.com/swiftlang/llvm-project.git \
 LLVM_VERSION=stable/21.x \
